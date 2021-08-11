d27982cffec2001742918e727a30809da51ea166
leyuri <leyuri97@gmail.com>
2021-08-11 18:51:06 +0900

    Add pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/:id">
+            <DetailPage />
+          </Route>
+          <Route path="/">
+            <MainPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/DetailPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DetailPage = () => {
+    return (
+        <div>
+            DetailPage
+        </div>
+    )
+};
+export default DetailPage;  
